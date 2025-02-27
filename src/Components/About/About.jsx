541946cf874d177184a692bd13e657391217ac8e
Amsan Naheswaran <amsan5941@gmail.com>
2025-02-27 11:06:18 -0500

    changed path for about img

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './About.css'
-import profile_img from '../../assets/VIX_8328.JPG'
+import profile_img from '../../assets/VIX_8328.jpg'
 
 const About = () => {
   const skills = [
